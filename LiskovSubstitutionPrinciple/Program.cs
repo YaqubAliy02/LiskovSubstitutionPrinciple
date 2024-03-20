bca41eb7d3d8b2ed0396a938de8ed55668f1c154
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-03-20 05:08:40 +0500

    Before Liskov

---
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,20 @@
+﻿using LiskovSubstitutionPrinciple.Services;
+
+namespace LiskovSubstitutionPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  ReadOnlyDocument readOnlyDocument = new ReadOnlyDocument("Hello", "text.txt");
+              readOnlyDocument.Save();
+              readOnlyDocument.Open();*/
+             string FILEPATH = "../../../Assets/read-only.txt";
+
+
+            File.Create(FILEPATH);
+            FileInfo fileInfo = new FileInfo(FILEPATH);
+            fileInfo.IsReadOnly = true;
+        }
+    }
+}
---
+++ LiskovSubstitutionPrinciple/Services/ReadOnlyDocument.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Enumeration;
+namespace LiskovSubstitutionPrinciple.Services
+{
+    internal class ReadOnlyDocument : Document
+    { 
+        public ReadOnlyDocument(string data, string fileName) : base(data, fileName) { }
+        public override void Save()
+        {
+          throw new Exception("Can't save a read-only document");
+        }
+    }
+      
+    
+}
